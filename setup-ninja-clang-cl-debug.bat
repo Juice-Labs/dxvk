Juice-Labs/dxvk-1

Updating setup files.

setup-ninja-clang-cl-debug.bat
@@ -1,0 +1,3 @@
+set CC=clang-cl
+set CXX=clang-cl
+meson setup --backend=ninja --buildtype=debug build\debug_vs2019

Juice-Labs/dxvk

setup-ninja-clang-cl-debug.bat
@@ -1,0 +1,3 @@
+set CC=clang-cl
+set CXX=clang-cl
+meson setup --backend=ninja --buildtype=debug build\debug