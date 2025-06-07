0bafddabc513259cefb772cffd032cc0181d4d9c
Dean Beeler <canadacow@gmail.com>
2025-06-06 17:14:04 -0700

    Simple merge portion

---
+++ setup-ninja-clang-cl-debug-win32.bat
@@ -0,0 +1,3 @@
+set CC=clang-cl
+set CXX=clang-cl
+meson setup --cross-file win32.txt --backend=ninja --buildtype=debug build\debug-win32 
---
+++ setup-ninja-clang-cl-release-win32.bat
@@ -0,0 +1,3 @@
+set CC=clang-cl
+set CXX=clang-cl
+meson setup --cross-file win32.txt --backend=ninja --buildtype=release build\release-win32 
